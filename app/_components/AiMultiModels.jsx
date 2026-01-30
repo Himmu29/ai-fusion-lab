a00112122536503a7080792503a7d807bd19f7f8
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2026-01-31 00:05:12 +0530

    Added Mutliple ai response

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -11,37 +11,50 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
-import { Lock, MessageSquare } from "lucide-react";
+import { Loader, Lock, MessageSquare } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { AiSelectedModelContext } from "@/context/AiSelectedModelContext";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/config/FirebaseConfig";
 import { useUser } from "@clerk/nextjs";
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 function AiMultiModels() {
-  const {user} = useUser();
+  const { user } = useUser();
   const [aiModelList, setAiModelList] = useState(AiModelList);
-  const { aiSelectedModels, setAiSelectedModels } = useContext(AiSelectedModelContext);
+  const { aiSelectedModels, setAiSelectedModels, messages, setMessages } = useContext(AiSelectedModelContext);
 
   const onToggleChange = (model, value) => {
     setAiModelList((prev) =>
       prev.map((m) => (m.model === model ? { ...m, enable: value } : m)),
     );
-  };
 
-  const onSelectValue = async (parentModel,value) =>{
-    setAiSelectedModels(prev=>({
+    // setAiSelectedModels((prev) =>
+    //   prev.map((m) => (m.model === model ? { ...m, enable: value } : m)),
+    // );
+
+    setAiSelectedModels((prev)=>({
       ...prev,
-      [parentModel]:{
-        modelId:value
+      [model]:{
+        ...(prev?.[model]??{}),
+        enable:value
       }
     }))
-    //Update to firebase database
-    const docRef = doc(db,"users",user?.primaryEmailAddress?.emailAddress);
-    await updateDoc(docRef,{
-      selectedModelPref:aiSelectedModels
-    })
-  }
+
+  };
+
+  console.log(aiSelectedModels);
+  
+  const onSelectValue = async (parentModel, value) => {
+    setAiSelectedModels((prev) => ({
+      ...prev,
+      [parentModel]: {
+        modelId: value,
+      },
+    }));
+    
+  };
 
   return (
     <div className="flex flex-1 h-[72vh] border-b">
@@ -63,12 +76,15 @@ function AiMultiModels() {
               />
 
               {model.enable && (
-                <Select defaultValue={aiSelectedModels[model.model].modelId} 
-                onValueChange={(value)=>onSelectValue(model.model, value)}
-                disabled = {model.premium}
+                <Select
+                  defaultValue={aiSelectedModels[model.model]?.modelId}
+                  onValueChange={(value) => onSelectValue(model.model, value)}
+                  disabled={model.premium}
                 >
                   <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder={aiSelectedModels[model.model].modelId} />
+                    <SelectValue
+                      placeholder={aiSelectedModels[model.model]?.modelId}
+                    />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup className="px-3">
@@ -92,8 +108,13 @@ function AiMultiModels() {
                       {model.subModel.map(
                         (subModel, index) =>
                           subModel.premium == true && (
-                            <SelectItem key={index} value={subModel.name} disabled={subModel.premium}>
-                              {subModel.name} {subModel.premium && <Lock className="h-4 w-4" />}
+                            <SelectItem
+                              key={index}
+                              value={subModel.name}
+                              disabled={subModel.premium}
+                            >
+                              {subModel.name}{" "}
+                              {subModel.premium && <Lock className="h-4 w-4" />}
                             </SelectItem>
                           ),
                       )}
@@ -120,6 +141,36 @@ function AiMultiModels() {
               <Button>
                 <Lock /> Upgrade to unlock
               </Button>
+            </div>
+          )}
+
+          {model.enable && (
+            <div className="flex-1 p-4">
+              <div className="flex-1 p-4 space-y-2">
+                {messages[model.model]?.map((m, i) => (
+                  <div
+                    className={`p-2 rounded-md ${m.role == "user" ? "bg-blue-100 text-blue-900" : "bg-gray-100 text-gray-900"}`}
+                  >
+                    {m.role == "assistant" && (
+                      <span className="text-sm text-gray-300">
+                        {m.model ?? model.model}
+                      </span>
+                    )}
+                    <div className="flex gap-3 items-center">
+                      {m.content == "loading" && (
+                        <>
+                          <Loader className="animate-spin" />{" "}
+                          <span>Thinking...</span>
+                        </>
+                      )}
+                    </div>
+                    {m.content != "loading" &&
+                    <Markdown remarkPlugins={[remarkGfm]}>
+                      {m.content}
+                    </Markdown>}
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </div>
