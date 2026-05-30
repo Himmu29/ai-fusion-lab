eb708b4c7d5866c861f8ca8e4248cc67f0684810
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2026-05-30 21:43:40 +0530

    Remove unused useTheme from page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
 import { Suspense } from "react";
 import Image from "next/image";
 import ChatInputBoxWrapper from "./_components/ChatInputBoxWrapper";
